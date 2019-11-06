60d1b4ab49b1af59deb52adc544eaed553ce4b22
Jens C <jca@osaft.org>
2019-11-06 17:37:33 +0100

    IWU specific changes.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.0.0
+VERSION=1.2.0
 
 all: clean build
 
--- chrome/content/iwutoolbox/toolbox.js
+++ chrome/content/iwutoolbox/toolbox.js
@@ -1,27 +1,63 @@
 Zotero.IWUToolbox = {
   init: function() {
-    //
     // Unregister callback when the window closes (important to avoid a memory leak)
     window.addEventListener(
-      "unload",
+      'unload',
       function(e) {
         Zotero.Notifier.unregisterObserver(notifierID);
       },
-      false
+      false,
     );
   },
 
-  copyToClipboard: function() {
-    // see zoteroPane.js - 	this.copySelectedItemsToClipboard = function (asCitations) {
-    ZoteroPane.copySelectedItemsToClipboard(true);
-  }
+  copyToClipboard: async function() {
+    var p = {
+      mode: 'bibliography',
+      contentType: 'text',
+      // id: 'http://www.zotero.org/styles/american-medical-association',
+      id: 'http://www.zotero.org/styles/apa-iwu',
+      locale: 'en-US',
+    };
+
+    if (Zotero.Styles.initialized) {
+      var handler = function(obj, worked) {};
+      var items = Zotero.getActiveZoteroPane().getSelectedItems();
+
+      //Zotero.logError('Items: ' + JSON.stringify(items, null, 4));
+
+      var result = [];
+
+      for (var i = 0; i < items.length; i++) {
+        var obj = JSON.parse(JSON.stringify(items[i]));
+        obj.bib = Zotero.QuickCopy.getContentFromItems(
+          [items[i]],
+          p,
+          handler,
+          false,
+        ).text;
+        result.push(obj);
+      }
+
+      var data = {data: result};
+
+      var str = JSON.stringify(data, null, 4);
+
+      //Zotero.logError('Result: ' + str);
+
+      Components.classes['@mozilla.org/widget/clipboardhelper;1']
+        .getService(Components.interfaces.nsIClipboardHelper)
+        .copyString(str);
+    } else {
+      Zotero.logError('Styles not ready when triggered.');
+    }
+  },
 };
 
 // Initialize the utility
 window.addEventListener(
-  "load",
+  'load',
   function(e) {
     Zotero.IWUToolbox.init();
   },
-  false
+  false,
 );
